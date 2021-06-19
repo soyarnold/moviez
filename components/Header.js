098fe0a3cd989c60c0008d1cc90d74106391da87
Arnold Perez <arnolddperez@gmail.com>
2021-06-19 14:27:25 -0500

    initial commit

---
+++ components/Header.js
@@ -0,0 +1,31 @@
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
+
+import { BiCameraMovie } from "react-icons/bi";
+
+import HeaderItem from "./HeaderItem";
+
+export default function Header() {
+  return (
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+        <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+      </div>
+      <div className="flex items-center">
+        <h1 className="font-logo font-bold text-3xl">MOVIEZ</h1>
+        <BiCameraMovie className="text-3xl" />
+      </div>
+    </header>
+  );
+}
